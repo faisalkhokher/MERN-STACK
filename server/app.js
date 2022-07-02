babd636a4c9983afb521fe687156f0d859861775
faisalkhokher50 <faisalkhokher50@gmail.com>
2022-07-02 23:17:20 +0500

    Express Router Middleware Setup & Getting Data Back From POSTMAN to Our MERN Application

--- server/app.js
+++ server/app.js
@@ -1,26 +1,33 @@
 require('dotenv').config()
  const express = require('express');
  const mongoose = require('mongoose');
-
-
+ // call express
  const app = express();
  
+ //! convert obj router to obj express
+ app.use(express.json());
+ 
+ //  Connect to router
+ app.use(require('./router/routes'));
+ 
  const PORT = process.env.PORT;
+ 
+ // MongoDB
  const mongo_dns = process.env.MONGO_DNS;
-
  const DB = mongo_dns;
-
+ 
  mongoose.connect(DB , { 
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true,
-        // useCreateIndex: true,
-        // useFindAndModify: false
- }).then((result) => {
-    console.log("Connected to MongoDB");
- }).catch((err) => {
-    console.log(err);
- });
-
+     // useNewUrlParser: true,
+     // useUnifiedTopology: true,
+     // useCreateIndex: true,
+     // useFindAndModify: false
+    }).then((result) => {
+        console.log("Connected to MongoDB");
+    }).catch((err) => {
+        console.log(err);
+    });
+    
+    
  app.listen(PORT , ()=> {
      console.log(`Server is running on port ${PORT}`);
  });
@@ -29,11 +36,9 @@ require('dotenv').config()
  const middleware = (req, res, next) => {
         console.log("Middleware");
         next();
-    }
+}
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+
 
 app.get('/about' , middleware , (req, res) => {
     res.send('About Page');
